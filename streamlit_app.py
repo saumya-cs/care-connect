a913ca8be893e8af46e9301bc07f759ed2993e92
Vrome-1 <vaanirometra@yahoo.com>
2024-10-19 11:44:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
 import streamlit as st
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
-st.set_page_config(
-    page_title= "Care Connect",
-    page_icon= "🤝",
-    layout= "center",
-    initial_sidebar_state="auto",
-)
