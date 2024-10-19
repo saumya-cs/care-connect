9a5efb5e56318b4b95c011f9bb3e50f457c7033e
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-19 11:46:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
+if page == "Mission"
+  st.write("Our Mission");
+  st.write("We aim to...");
