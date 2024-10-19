db00263f8d86bcf45f6af37221d340367979380f
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-19 12:02:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
 
 if page == "Mission":
+  st.title('Welcome! :heart:');
   st.subheader("Welcome!");
   st.write("Welcome to Care Connect! This website is all about support and community - we aim to help patients find and connect with others who understand what they are going through.");
   st.subheader("Our Motivation");
