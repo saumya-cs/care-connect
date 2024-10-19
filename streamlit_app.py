933ee77cd8379f2cd8d17013e3e4e34d3ee68dc6
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-19 11:39:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
 st.set_page_config(
     page_title= "Care Connect",
     page_icon= "🤝",
